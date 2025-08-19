13c7085db0d882ba9406e83ab485cc1c3c393b02
kmafeni04 <komemafeni944@gmail.com>
2025-08-19 13:27:08 +0100

    Update config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export", // <=== enables static exports
+  basePath: "/didi",
+  assetPrefix: "/didi/",
+  reactStrictMode: true,
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
